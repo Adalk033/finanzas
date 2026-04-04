a67ad7e83f936cbc38129dce7609e8fd443896bb
Adalk033 <adal_garcia1992@hotmail.com>
2026-04-03 22:55:43 -0600

    feat: add electron configuration and integrate vite plugins for electron support

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,41 @@
 import { defineConfig } from 'vite'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
+import electron from 'vite-plugin-electron'
+import renderer from 'vite-plugin-electron-renderer'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    babel({ presets: [reactCompilerPreset()] })
+    babel({ presets: [reactCompilerPreset()] }),
+    electron([
+      {
+        entry: 'electron/main.ts',
+        onstart({ startup }) {
+          startup()
+        },
+        vite: {
+          build: {
+            outDir: 'dist-electron',
+            rollupOptions: {
+              external: ['better-sqlite3'],
+            },
+          },
+        },
+      },
+      {
+        entry: 'electron/preload.ts',
+        vite: {
+          build: {
+            outDir: 'dist-electron',
+            rollupOptions: {
+              external: ['better-sqlite3'],
+            },
+          },
+        },
+      },
+    ]),
+    renderer(),
   ],
 })
